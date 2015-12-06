8bfafd39a6a82ea6b70667492c5db2a664344493
Morgan Carter <morgancarter1@gmail.com>
2015-12-06 21:49:01 +0100

    retry google analytics setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,14 @@
 try{Typekit.load({ async: true });}catch(e){}
 
 
-/* Fast Click Initialise */
-// FastClick.attach(document.body);
+/* Google Analytics Initialise */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-63372069-2', 'auto');
+ga('send', 'pageview');
 
 
 /* Instant Click */
